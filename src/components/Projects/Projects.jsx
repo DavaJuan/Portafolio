b57a1003a9c98662cacfed229dfcf4768ee2718b
DavaJuan <davalosjuanpablo038@gmail.com>
2023-02-28 11:54:50 -0300

    add cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,27 +1,13 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
-import Pokmania from './img/Pokemania.png';
-import WeatherApp from './img/WeatherApp.png';
+import Cards from './Card Cards/Cards';
 import './Projects.css';
+
 
 const Projects = () => {
   return (
     <div className='container-proyects'> 
-      <div className="container-cards">
-        <div className='proyect-card'>
-          <h3>Pokemani</h3>
-          <img src={Pokmania} alt="" width={'300px'}/>
-          <p>This proyect was created durin my henry</p>
-          <button><a href="#">Go to know it!</a></button>
-        </div>
-
-        <div className='proyect-card'>
-          <h3>Wheater App</h3>
-          <img src={WeatherApp} alt="" width={'300px'}/>
-          <p>This proyect was created durin my henry</p>
-          <button><a href="#">Go to know it!</a></button>
-        </div>
-      </div>
+      <Cards />
     </div>
   );
 }
