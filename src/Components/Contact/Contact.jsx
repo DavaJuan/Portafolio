fda8da432793dc222dd923d444b49f5c3dbbc4dd
DavaJuan <davalosjuanpablo038@gmail.com>
2023-05-03 13:08:04 -0300

    form controled

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,73 +1,130 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import { BsWhatsapp,BsInstagram,BsSend,BsArrowRight } from 'react-icons/bs';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { useForm } from "react-hook-form";
+import { BsWhatsapp, BsInstagram, BsSend, BsArrowRight } from "react-icons/bs";
 
-import './Contact.css';
+import "./Contact.css";
 
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
   const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const onSubmit = async () => {
+    try {
+      await emailjs
+        .sendForm(
+          "service_ldaidad",
+          "template_0fjt6az",
+          form.current,
+          "vsv1o8h3GCgxnNB-l"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            alert("Message sent!");
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
 
-    emailjs.sendForm(
-      'service_ldaidad', 
-      'template_0fjt6az',
-       form.current,
-      'vsv1o8h3GCgxnNB-l')
-      .then((result) => {
-          console.log(result.text);
-          alert('Message sent!');
-      }, (error) => {
-          console.log(error.text);
-      });
+      reset();
+    } catch (error) {
+      console.error("error", error);
+    }
   };
 
+  const regex =
+    /^(([^<>()[].,;:\s@"]+(.[^<>()[].,;:\s@"]+)*)|(".+"))@(([^<>()[].,;:\s@"]+.)+[^<>()[].,;:\s@"]{2,})$/i;
 
   return (
-    <div className='contAll'>
+    <div className="contAll">
       <h3>Contact Me</h3>
-      <div className='contactGrid'>
-        <div className='contacts'>
-          <div className='contact'>
-            <BsInstagram className='ContInco'/>
-            <p className='pName'>Instagram</p>
+      <div className="contactGrid">
+        <div className="contacts">
+          <div className="contact">
+            <BsInstagram className="ContInco" />
+            <p className="pName">Instagram</p>
             <p>@jpd_web_developer.23</p>
-            <p>Check my Instagram <BsArrowRight /></p>
+            <div className="containerA">
+              <a className="multimedia" href="" target="_blank">
+                Check my Instagram
+              </a>
+              <BsArrowRight className="rowRight" />
+            </div>
           </div>
-          <div className='contact'>
-            <BsWhatsapp className='ContInco'/>
-            <p className='pName'>WhatsApp</p>
+          <div className="contact">
+            <BsWhatsapp className="ContInco" />
+            <p className="pName">WhatsApp</p>
             <p>+54 1153754846</p>
-            <p>Write me <BsArrowRight /></p>
+            <div className="containerA">
+              <a
+                className="multimedia"
+                href="https://api.whatsapp.com/send?phone=541153754846"
+                target="_blank"
+              >
+                Write me
+              </a>
+              <BsArrowRight className="rowRight" />
+            </div>
           </div>
         </div>
-        <div className='form'>
-          <h4>Send me an e-mail</h4>    
-          <form className='inputs' ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input 
-              type="text" 
-              name="user_name" 
-              placeholder='Insert your name...'
+        <div className="form">
+          <h4>Send me an e-mail</h4>
+          <form className="inputs" ref={form} onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="name">Name</label>
+            <input
+              {...register("name", {
+                required: true,
+              })}
+              id="name"
+              type="text"
             />
-            <label>Email</label>
-            <input 
-              type="email" 
-              name="user_email" 
-              placeholder='Insert your email...'
+            {errors.name?.type === "required" && (
+              <p className="errorMessage">required information</p>
+            )}
+            <label htmlFor="email">Email</label>
+            <input
+              {...register("email", {
+                required: true,
+                pattern: regex,
+              })}
+              id="email"
+              type="email"
             />
-            <label>Message</label>
-            <textarea name="message" cols="30" rows="10" placeholder='Write your massage...'/>
-            <div className='buttonContainer'>
-              <button className='buttonSend' type="submit" value="Send">Send <BsSend /></button>
+            {errors.email?.type === "pattern" ? (
+              <p className="errorMessage">Email invalid</p>
+            ) : (
+              errors.email?.type === "required" && (
+                <p className="errorMessage">required information</p>
+              )
+            )}
+            <label htmlFor="message">Message</label>
+            <textarea
+              {...register("message", {
+                required: true,
+              })}
+              id="message"
+              cols="30"
+              rows="10"
+            ></textarea>
+            {errors.message?.type === "required" && (
+              <p className="errorMessage">required information</p>
+            )}
+            <div className="divInpuntSend">
+              <input id="inputSubmit" type="submit" value="Send" />
             </div>
           </form>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Contact;
